54ade1f742f695b8567a0c00dbf0550b52b64116
Martes <96113508+martesi@users.noreply.github.com>
2026-01-23 08:52:37 -0500

    chore: update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,6 +11,11 @@ export default defineConfig([
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
